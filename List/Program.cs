bca4a2488f08e0eb5d80af38e660624d42defdb9
H4N4K8 <hannah.francis03@gmail.com>
2024-03-18 08:26:15 -0400

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> numbers = new List<string> { "1", "2", "3", "4", "5"};
+
+            Console.WriteLine($"There original list has {numbers.Count()} members");
+            string[] memberArr = new string[] { "6", "7", "8"};
+            numbers.AddRange(memberArr);
+            Console.WriteLine($"The new list now has {numbers.Count()} members");
+
+            Console.WriteLine("Original Numbers");
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            numbers.Remove("3");
+
+            Console.WriteLine($"The revised list now has {numbers.Count()} members");
+            Console.WriteLine("Revised Numbers");
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            numbers.Sort();
+            Console.WriteLine("Sorted Numbers");
+            foreach (string n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            numbers.Reverse();
+            Console.WriteLine("Reverse Numbers");
+            foreach (string n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
